7145a8b5240df5d95b59466767aea5e441d50cce
Nkurumwa <nkurumwa51@gmail.com>
2019-11-25 19:36:39 +0300

    imports login required

--- awwards/views.py
+++ awwards/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from .models import Project, Rate, Review
 from django.contrib.auth.models import User
 from .forms import( UserRegisterForm, UserUpdateForm, ProfileUpdateForm, ProjectPostForm, RateForm, ReviewForm)
-
+from django.contrib.auth.decorators import login_required
 
 
 
