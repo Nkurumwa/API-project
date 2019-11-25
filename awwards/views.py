f7cffc9e8808b85b59f808be80ba6d671e1abc93
Nkurumwa <nkurumwa51@gmail.com>
2019-11-25 19:35:33 +0300

    import files

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import Http404
+from .models import Project, Rate, Review
+from django.contrib.auth.models import User
+from .forms import( UserRegisterForm, UserUpdateForm, ProfileUpdateForm, ProjectPostForm, RateForm, ReviewForm)
 
-# Create your views here.
+
+
+
+
